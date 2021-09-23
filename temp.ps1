8ad95b6339bbc6a438a378cefbe18df60aa75e76
Gabriel fresan <69011585+tec-fox@users.noreply.github.com>
2021-09-22 21:15:19 -0300

    Add files via upload

--- temp.ps1
+++ temp.ps1
@@ -2,6 +2,6 @@ cd $env:APPDATA;
 cd .minecraft;
 new-item ./mods/old-mods;
 move-item -Path ./mods/* -destination ./mods/old-mods;
-Invoke-WebRequest -Uri https://github.com/tec-fox/repos-gfs/raw/main/mods.zip -OutFile mods.zip;
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/tec-fox/repos-gfs/main/mods.zip -OutFile mods.zip;
 Expand-Archive ./mods.zip;
 Remove-Item –path ./mods.zip
