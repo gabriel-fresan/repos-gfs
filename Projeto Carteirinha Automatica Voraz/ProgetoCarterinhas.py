481ea6c86f45774e32f0ff501fc119c930a77333
Gabriel fresan <69011585+tec-fox@users.noreply.github.com>
2022-03-04 11:43:29 -0300

    Update ProgetoCarterinhas.py

--- Projeto Carteirinha Automatica Voraz/ProgetoCarterinhas.py
+++ Projeto Carteirinha Automatica Voraz/ProgetoCarterinhas.py
@@ -3,6 +3,7 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import os
+import easygui_qt as easygui
 
 def clearConsole():
     command = 'clear'
@@ -17,7 +18,7 @@ auto = auto.resize((2600 , 3600))
 
 myFont = ImageFont.truetype('NewsGothicBT.ttf', 87)
 
-cTipo = input('A Carterinha Sera \n (1) Auto, (2) Residencial ou (3) Empresarial ? \n \n')
+cTipo = easygui.get_choice('A Carterinha Sera ? \n \n' , 'tipo de carteirinha' , ['1. Auto' , '2. Residencial' , '3. Empresarial'])
 
 clearConsole()
 
@@ -26,69 +27,73 @@ def cMaker(img) :
 
     #Area que Vai Receber a Informação 
     if True :
-        nome = input('Qual o Nome ? \n \n')
+        nome = easygui.get_string('Qual o Nome ? \n \n')
         clearConsole()
-        cpf = input('Qual o CPF ? \n \n')
+        cpf = easygui.get_string('Qual o CPF ? \n \n')
         clearConsole()
-        seguradora = input('Qual a Seguradora ? \n \n')
+        seguradora = easygui.get_string('Qual a Seguradora ? \n \n')
         clearConsole()
-        nSeguradora = input('Qual o Numero da Seguradora ? \n \n')
+        nSeguradora = easygui.get_string('Qual o Numero da Seguradora ? \n \n')
         clearConsole()
-        nApolice = input('Qual o Numero da Apolice \n \n')
+        nApolice = easygui.get_string('Qual o Numero da Apolice \n \n')
         clearConsole()
-        cResponsavel = input('Qual o Coretor Responsavel ? \n (1) Vera    (2) Renata \n \n')
+        cResponsavel = easygui.get_choice('Qual o Coretor Responsavel ? \n \n','Coretor' , ['1. Vera Viana' , '2. Renata Dutra'])
+        cResponsavel = cResponsavel.replace('1. ','')
+        cResponsavel = cResponsavel.replace('2. ','')
         clearConsole()
-        mVeiculo = input('Qual o Modelo do Veiculo ? \n \n')
+        mVeiculo = easygui.get_string('Qual o Modelo do Veiculo ? \n \n')
         clearConsole()
-        pVeiculo = input('Qual a Placa do Veiculo ? \n \n')
+        pVeiculo = easygui.get_string('Qual a Placa do Veiculo ? \n \n')
         clearConsole()
-        cep = input('Qual o CEP de Pernoite ? \n \n')
+        cep = easygui.get_string('Qual o CEP de Pernoite ? \n \n')
         clearConsole()
-        codC = input('Qual o Código do Cliente ? \n \n')
+        codC = easygui.get_string('Qual o Código do Cliente ? \n \n')
         clearConsole()
-        codDoc = input('Qual o Código do Documento ? \n \n')
+        codDoc = easygui.get_string('Qual o Código do Documento ? \n \n')
         clearConsole()
-        vigencia = input('Qual a Vigencia ? \n \n ')
+        vigencia = easygui.get_string('Qual a Vigencia ? \n \n ')
     
     
+    def drawImage() :
+        img.text((330, 480), "Cliente: " + nome.title(), font=myFont, fill=(255, 255, 255))
+        
+        img.text((330, 680), "CPF: " + cpf.title(), font=myFont, fill=(255, 255, 255))
+        
+        img.text((330, 880), "Seguradora: " + seguradora.title(), font=myFont, fill=(255, 255, 255))
+        
+        img.text((330, 1080), "Ramo: Automóvel" , font=myFont, fill=(255, 255, 255))
+        
+        img.text((1530, 680), "Código do Cliente: " + codC.title(), font=myFont, fill=(255, 255, 255))
+        
+        img.text((1530, 880), "Vigência: " + vigencia.upper(), font=myFont, fill=(255, 255, 255))
+        
+        img.text((1530, 1080), "Apólice: " + nApolice.title(), font=myFont, fill=(255, 255, 255))
+        
+        img.text((330, 2515), "Coretora Responsável: " + cResponsavel.title(), font=myFont, fill=(1, 70, 185), stroke_width=2)
+        
+        img.text((330, 2665), "Telefone Seguradora: " + nSeguradora.title(), font=myFont, fill=(1, 70, 185))
+        
+        img.text((330, 2785), "Veículo: " + mVeiculo.title(), font=myFont, fill=(1, 70, 185))
+        
+        img.text((330, 2905), "Placa: " + pVeiculo.upper(), font=myFont, fill=(1, 70, 185))
+        
+        img.text((330, 3025), "CEP de Pernoite: " + cep.title(), font=myFont, fill=(1, 70, 185))
+        
+        img.text((1530, 2905), "Código de Doc.: " + codDoc.title(), font=myFont, fill=(1, 70, 185))
     
-    
-    img.text((330, 480), "Cliente: " + nome.title(), font=myFont, fill=(255, 255, 255))
-    
-    img.text((330, 680), "CPF: " + cpf.title(), font=myFont, fill=(255, 255, 255))
-    
-    img.text((330, 880), "Seguradora: " + seguradora.title(), font=myFont, fill=(255, 255, 255))
-    
-    img.text((330, 1080), "Ramo: Automóvel" , font=myFont, fill=(255, 255, 255))
-    
-    img.text((1530, 680), "Código do Cliente: " + codC.title(), font=myFont, fill=(255, 255, 255))
-    
-    img.text((1530, 880), "Vigência: " + vigencia.upper(), font=myFont, fill=(255, 255, 255))
-    
-    img.text((1530, 1080), "Apólice: " + nApolice.title(), font=myFont, fill=(255, 255, 255))
-    
-    img.text((330, 2515), "Coretora Responsável: " + cResponsavel.title(), font=myFont, fill=(1, 70, 185), stroke_width=2)
-    
-    img.text((330, 2665), "Telefone Seguradora: " + nSeguradora.title(), font=myFont, fill=(1, 70, 185))
-    
-    img.text((330, 2785), "Veículo: " + mVeiculo.title(), font=myFont, fill=(1, 70, 185))
-    
-    img.text((330, 2905), "Placa: " + pVeiculo.upper(), font=myFont, fill=(1, 70, 185))
-    
-    img.text((330, 3025), "CEP de Pernoite: " + cep.title(), font=myFont, fill=(1, 70, 185))
-    
-    img.text((1530, 2905), "Código de Doc.: " + codDoc.title(), font=myFont, fill=(1, 70, 185))
-    
+    drawImage()
 
 
 
-if cTipo in '1' or "auto" in cTipo.lower():
+if '1' in cTipo or "auto" in cTipo.lower():
 
     imgDr = ImageDraw.Draw(auto)
     
     cMaker(imgDr)
     
-    auto.save("./#30.Carteirinha-do-Cliente.png")
+    onde = easygui.get_save_file_name()
+    
+    auto.save(onde)
     
     auto.show()
     
